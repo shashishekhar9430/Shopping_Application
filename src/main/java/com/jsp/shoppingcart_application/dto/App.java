1d70f17381714385359f4c3e512dacf4bb993d9a
unknown <shashishekhar9430>
2024-10-13 14:43:02 +0530

    Final commit

---
+++ src/main/java/com/jsp/shoppingcart_application/dto/App.java
@@ -0,0 +1,13 @@
+package com.jsp.shoppingcart_application.dto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
